0936f66dddf2b4f28dc238b04a9a4aaa75f42abc
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-08-28 15:06:38 +0300

    Добавлена проверка количества билетов на 1, если да, то человек катается на аттракционе, и идет домой. Именьшила количество билетов у Person для лучшей картинки

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,24 @@ public class Main {
 
         while (!clients.isEmpty()) {
             Person c = clients.poll();
-            if (c.tickets != 0) {
+            if (c.tickets != 1) {
                 c.tickets--;
                 System.out.println(c.name + " " + c.surname + " посетил(а) аттракцион.");
                 clients.add(c);
+            } else {
+                c.tickets--;
+                System.out.println(c.name + " " + c.surname + " посетил(а) аттракцион, билеты закончились.");
             }
         }
     }
 
     public static List<Person> generateClients() {
         List<Person> list = new LinkedList<>();
-        list.add(new Person("Петя", "Фролов", 5));
-        list.add(new Person("Оля", "Петрова", 15));
+        list.add(new Person("Петя", "Фролов", 3));
+        list.add(new Person("Оля", "Петрова", 7));
         list.add(new Person("Юля", "Семенова", 3));
-        list.add(new Person("Вова", "Викторов", 10));
-        list.add(new Person("Жанна", "Иванова", 21));
+        list.add(new Person("Вова", "Викторов", 1));
+        list.add(new Person("Жанна", "Иванова", 10));
         return list;
     }
 }
